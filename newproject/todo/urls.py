ebbc7c446b17aa7b526389789f843690657c3d68
Nikkole Spurgeon <71410396+NikkoleAshleigh@users.noreply.github.com>
2021-03-19 13:59:15 -0700

    Delete newproject directory

--- newproject/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from .viewsets import TodoViewset
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-router.register('todos', TodoViewset)
-
-urlpatterns = router.urls
--- newproject/todo/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets, permissions
-from . import models
-from . import serializers
-
-
-class TodoViewset(viewsets.ModelViewSet):
-    queryset = models.Todo.objects.all()
-    serializer_class = serializers.TodoSerializer
